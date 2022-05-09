33cbbbf0c0bdfd1da8278f2d2eec1aef5c1cb7fe
Vishalj633 <jagdalevishal633@gmail.com>
2022-05-09 09:11:53 +0530

    Bean Scopes

---
+++ spring-demo-one/src/com/farm/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,22 @@
+package com.farm.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+		
+		// Load spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyPractice());
+		
+		// Close the context
+		context.close();
+
+	}
+
+}
